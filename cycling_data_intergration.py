1e95e22d0168337304f71ec8abd42af990ec8206
Jonathon <jonathon.longden@anu.edu.au>
2021-09-28 14:45:28 +1000

    starting of an export and explore

--- cycling_data_intergration.py
+++ cycling_data_intergration.py
@@ -1,6 +1,13 @@
 # this is a working file where I will export some data for a better view
+import openpyxl
+
+
 from cycling_load_data import *
+data_index_path = Path(DATA_FOLDER) / DATA_INDEX
 
+working = load_data(data_index_path)
 
-print(Rainfall)
-print(Suburb)
+#working['rainfall']['canberra airport'].to_excel(r"C:\Users\Admin\OneDrive\Documents\assignment 2 working\rainfall.xlsx")
+working['crash'].to_excel(r"C:\Users\Admin\OneDrive\Documents\assignment 2 working\crash.xlsx")
+working['streetlight'].to_excel(r"C:\Users\Admin\OneDrive\Documents\assignment 2 working\streetlight.xlsx")
+#print(working['cyclist'])
