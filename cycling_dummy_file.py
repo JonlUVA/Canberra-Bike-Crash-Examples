2e553eb67355d2549f6469817429b8b041742393
tim-arney <85653684+tim-arney@users.noreply.github.com>
2021-09-30 21:59:46 +1000

    integrated data analysis with disk dump to read local on subsequent executions

--- cycling_dummy_file.py
+++ cycling_dummy_file.py
@@ -15,6 +15,9 @@ from pathlib import Path
 from cycling_globals import *
 from cycling_download_data import *
 from cycling_load_data import *
+from cycling_data_integration import *
+from cycling_helper_functions import *
+
     
 
 ##############################################################################
@@ -23,39 +26,83 @@ from cycling_load_data import *
 
 if __name__ == '__main__':
     
-    ##########################################################################
-    #                CHECK LOCAL DATA OTHERWISE DOWNLOAD DATA                #
-    ##########################################################################
-
+    crash_file = 'crash_data.xlsx'
+    cyclists_file = 'cyclist_data.xlsx'
+    
+    crash_path = Path(DATA_FOLDER) / crash_file
+    cyclists_path = Path(DATA_FOLDER) / cyclists_file
+    
     print()
-    print('Checking Local Data Sources')
-    print()  
-
-    data_index_path = Path(DATA_FOLDER) / DATA_INDEX
-
-    if not check_local_data(data_index_path):
+    
+    ##########################################################################
+    #        CHECK FOR LOCAL ANALYSED DATA OTHERWISE FULL SYSTEM CHECK       #
+    ##########################################################################
+    
+    if file_exists(crash_path) and file_exists(cyclists_path):
+        print('Loading Data Analysis')
         print()
-        print('Downloading Data')
         
-        download_all_data(DATA_SOURCES, DATA_FOLDER, DATA_INDEX)
+        integrated_data = {}
+        integrated_data['crash_data'] = read_excel_to_df(crash_path)
+        integrated_data['estimated_cycle'] = read_excel_to_df(cyclists_path)
+        
+        print('All data loaded')
     else:
+            
+        ##########################################################################
+        #             CHECK LOCAL RAW DATA OTHERWISE DOWNLOAD DATA               #
+        ##########################################################################
+    
         print()
-        print('All data sources found')
+        print('Checking Local Data Sources')
+        print()  
+    
+        data_index_path = Path(DATA_FOLDER) / DATA_INDEX
+    
+        if not check_local_data(data_index_path):
+            print()
+            print('Downloading Data')
+            
+            download_all_data(DATA_SOURCES, DATA_FOLDER, DATA_INDEX)
+        else:
+            print()
+            print('All data sources found')
+    
+        
+        ##########################################################################
+        #                              LOAD RAW DATA                             #
+        ##########################################################################
+        print()
+        print('Loading Data')
+        print()  
+        
+        data = load_data(data_index_path)
+        print()
+        print('All data sources loaded')
+        
+        ##########################################################################
+        #                    ANALYSE RAW DATA AND WRITE TO DISK                  #
+        ##########################################################################
+        
+        print()
+        print('Analysing Data')
+        print()
+        
+        integrated_data = integration(data)
+        write_df_to_excel(integrated_data['crash_data'], crash_path)
+        write_df_to_excel(integrated_data['estimated_cycle'], cyclists_path)
+        
+        print('All data analysed')
 
     
     ##########################################################################
-    #                                LOAD DATA                               #
+    #                              VISUALISE DATA                            #
     ##########################################################################
     print()
-    print('Loading Data')
-    print()  
+    print('Visualising Data')
+    # print()  
     
-    data = load_data(data_index_path)
+    pass
     print()
-    print('All data sources loaded')
+    print('...nothing to see here (yet)')
     
-    ##########################################################################
-    #                         ANALYSE / VISUALISE DATA                       #
-    ##########################################################################
-    
-    # do what you will with 'data' from here
