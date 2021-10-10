81454f38dabcef7a401842d4119ea96399403b96
Hugh Porter <u7398670@anu.edu.au>
2021-10-10 15:44:28 +1100

    Fixed Main Issues

--- apps/cycling_visual_global_functions.py
+++ apps/cycling_visual_global_functions.py
@@ -11,13 +11,6 @@ def get_data_for_vis(return_both):
     :param return_both: Are both cyclist and crashes data required for the vis 0 = no, 1 = yes
     :return: the dataset(s) required
     """
-    """
-        for i in os.listdir('apps/temp_data'):
-        if i.startswith('temp_crashes_'):
-            crashes_raw_data = pd.read_csv('apps/temp_data/'+i)
-        elif i.startswith('temp_cyclists_'):
-            cyclist_raw_data = pd.read_csv('apps/temp_data/'+i)
-    """
     crashes_raw_data = pd.read_csv('data/crashes.csv')
     cyclist_raw_data = pd.read_csv('data/cyclists.csv')
 
