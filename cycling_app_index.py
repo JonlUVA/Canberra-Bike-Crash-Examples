5c8b840a056c46aac9618c893eed63fb54b2eb92
Hugh Porter <u7398670@anu.edu.au>
2021-10-10 14:16:24 +1100

    Add comments to cycling_app_index.py

--- cycling_app_index.py
+++ cycling_app_index.py
@@ -1,29 +1,26 @@
-import json
-import sys
-
-import plotly as plt
-import plotly.express as px
-import pandas as pd
-import csv
-import dash
 from dash import html
 from dash import dcc
 from dash.dependencies import Input, Output, State
 from flask import request
 
+#   Importing the app
 from cycling_dashboard_app import app, server
 
+#   Importing all the visuals
 from apps import cycling_visual_one
 from apps import cycling_visual_two
 from apps import cycling_visual_three
 from apps import cycling_visual_four
 
-
+######################################################################
+#                          SETTING UP HTML                           #
+######################################################################
 app.layout = html.Div(
     id='main',
     children=[
-        #pathname being used to select first visual
+        #   Pathname being used to select first visual
         dcc.Location(id='url', refresh=False, pathname='/apps/visual_one'),
+        #   Side Navigation
         html.Div(
             id='side_menu',
             children=[
@@ -35,6 +32,7 @@ app.layout = html.Div(
                 dcc.Link('Close', href='/close')
             ]
         ),
+        #   Visuals from files go here (Output Div)
         html.Div(
             id='output',
             children=[]
@@ -42,12 +40,22 @@ app.layout = html.Div(
     ]
 )
 
-
+######################################################################
+#                          SETTING UP APP                            #
+######################################################################
+"""
+    Based on the pathname the app will return the variable var_dashboard
+    as children in the output div
+"""
 @app.callback(
     Output(component_id='output', component_property='children'),
     [Input(component_id='url', component_property='pathname')]
 )
 def display_dashboard(pathname):
+    """
+    :param pathname: The name of the visual the user is looking at
+    :return: the visuals from py file
+    """
     if pathname == '/apps/visual_one':
         return cycling_visual_one.var_dashboard
     elif pathname == '/apps/visual_two':
@@ -66,4 +74,3 @@ def display_dashboard(pathname):
 
 if __name__ == '__main__':
     app.run_server(debug=True, dev_tools_ui=False)
-    #, dev_tools_ui=False
