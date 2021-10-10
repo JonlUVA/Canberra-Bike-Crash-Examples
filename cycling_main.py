0ba414cc8e342e587356d7105a9b4cb0d30d0d71
Hugh Porter <u7398670@anu.edu.au>
2021-10-10 16:28:15 +1100

    Updated Run_Server

--- cycling_main.py
+++ cycling_main.py
@@ -191,7 +191,7 @@ if __name__ == '__main__':
 
     from cycling_app_index import *
 
-    app.run_server(debug=True, dev_tools_ui=False, dev_tools_silence_routes_logging=True, use_reloader=False)
+    app.run_server(debug=False, dev_tools_silence_routes_logging=True, use_reloader=True)
 
     print()
     print('END :)')
